03466761cbee371a77585fd49d6e90d59fc07bf6
lhaggarty <lhaggarty@me.com>
2016-04-05 22:42:49 -0400

    adding files

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require($DOCUMENT_ROOT . "layout.php");
+?>
---
+++ landingPage.php
@@ -0,0 +1,43 @@
+<?php require($DOCUMENT_ROOT . "_navbar.html"); ?>
+<div class="">
+  <div id="graphSection">
+    <div class="text-center">
+      <h1 style="padding-top:100px">Share your Files with Ease</h1>
+      <div class="row">
+        <div style="element-center">
+          <p class="side-padding" style="font-size:125%">We push ourselves to new levels of inspirational excellence with each
+            project and sweat creativity</p>
+        </div>
+      </div>
+      <br>
+
+      <button class="btn btn-default">Buy Now | ‎€23</button>
+
+  </div>
+
+    <?php require($DOCUMENT_ROOT . "_graph.html"); ?>
+
+    <div class="row">
+      <div class="col-sm-offset-3 col-sm-6 text-center" style="padding-top:50px">
+        <p style="font-size:170%">We push ourselves to new levels of inspirational excellence with
+          each project and sweat creativity</p>
+        <button class="btn btn-success">Buy now for ‎€23</button>
+      </div>
+    </div>
+
+  </div>
+
+  <hr>
+
+  <div id="testimonySection">
+    <?php require($DOCUMENT_ROOT . "_testimonyCarousel.html"); ?>
+  </div>
+
+  <div>
+    <?php require($DOCUMENT_ROOT . "_pricing.html"); ?>
+  </div>
+
+  <div>
+    <?php require($DOCUMENT_ROOT . "_emailSignUp.html"); ?>
+  </div>
+</div>
